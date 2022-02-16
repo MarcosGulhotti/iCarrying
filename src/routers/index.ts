6db9e7cee8c41483d77e90e090e50e19fa42c2a4
Maikol Moraes <maikolmoraesas@gmail.com>
2022-02-16 14:13:03 -0300

    feat: creating login market

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
+import { loginRouter } from "./login.router";
 import { marketRouter } from "./market.router";
 
 export const initializerRouter = (app: Express) => {
     app.use("/market", marketRouter());
+    app.use("/login", loginRouter());
 }
