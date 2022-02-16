317f36c9057b7013ab72f4f27c6daa4a2dd32c6e
Markinn <marcosmvg77@gmail.com>
2022-02-16 17:10:42 -0300

    feat: product routes

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -1,12 +1,33 @@
 import { Router } from "express";
-import { createProductController } from "../controllers/product.controller";
+import {
+  createProductController,
+  deleteProductController,
+  listAllProductsController,
+  listAllProductsFromSupplierController,
+  listProductByIdController,
+  updateProductController,
+} from "../controllers/product.controller";
+import { isAuthenticated } from "../middlewares/authentication.middleware";
 import { validate } from "../middlewares/validation.middleware";
 import { CreateProductSchema } from "../schemas/productSchema";
 
 const router = Router();
 
 export const productRouter = () => {
-  router.post("/suplier/:id", validate(CreateProductSchema), createProductController);
+  router.post(
+    "/supplier/:id",
+    validate(CreateProductSchema),
+    createProductController
+  );
+  router.get("/:id", isAuthenticated, listProductByIdController);
+  router.get("/", isAuthenticated, listAllProductsController);
+  router.get(
+    "/supplier/:id",
+    isAuthenticated,
+    listAllProductsFromSupplierController
+  );
+  router.patch("/:id", isAuthenticated, updateProductController);
+  router.delete("/:id", isAuthenticated, deleteProductController);
 
   return router;
 };
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -10,6 +10,13 @@ interface IregisterProduct {
   image: string;
 }
 
+interface IupdateProduct {
+  name?: string;
+  price?: string;
+  description?: string;
+  image?: string;
+}
+
 const removeSuplier = (product: Product) => {
   const { id, name, price, description, image } = product;
 
@@ -52,6 +59,118 @@ export const createProduct = async (
     await productRepository.save(product);
 
     return removeSuplier(product);
+  } catch (error) {
+    throw new AppError((error as any).message, 400);
+  }
+};
+
+export const listProductById = async (id: string) => {
+  try {
+    const productRepository = getRepository(Product);
+
+    const product = await productRepository.findOne(id);
+
+    if (product === undefined) {
+      throw new AppError("Product not exists", 400);
+    }
+
+    return product;
+  } catch (error) {
+    throw new AppError((error as any).message, 400);
+  }
+};
+
+export const listAllProducts = async () => {
+  try {
+    const productRepository = getRepository(Product);
+
+    const products = await productRepository.find();
+
+    return products;
+  } catch (error) {
+    throw new AppError((error as any).message, 400);
+  }
+};
+
+export const listAllProductsFromSupplier = async (supplierID: string) => {
+  try {
+    const productRepository = getRepository(Product);
+
+    const products = await productRepository.find({
+      where: { suplier: supplierID },
+    });
+
+    return products;
+  } catch (error) {
+    throw new AppError((error as any).message, 400);
+  }
+};
+
+export const updateProduct = async (
+  data: IupdateProduct,
+  id: string,
+  supplierID: string
+) => {
+  try {
+    const productRepository = getRepository(Product);
+    const suplierRepository = getRepository(Supplier);
+
+    const suplier = await suplierRepository.findOne(supplierID);
+
+    if (suplier === undefined) {
+      throw new AppError("Suplier not exists", 400);
+    }
+
+    const product = await productRepository.findOne({
+      where: {
+        suplier: suplier,
+        id: id,
+      },
+    });
+    if (product === undefined) {
+      throw new AppError(
+        "Product not exists or its not from this supplier",
+        400
+      );
+    }
+
+    const newProduct = await productRepository.save({
+      ...product,
+      ...data,
+    });
+
+    return newProduct;
+  } catch (error) {
+    throw new AppError((error as any).message, 400);
+  }
+};
+
+export const deleteProduct = async (id: string, supplierID: string) => {
+  try {
+    const productRepository = getRepository(Product);
+    const suplierRepository = getRepository(Supplier);
+
+    const suplier = await suplierRepository.findOne(supplierID);
+
+    if (suplier === undefined) {
+      throw new AppError("Suplier not exists", 400);
+    }
+
+    const product = await productRepository.findOne({
+      where: {
+        suplier: suplier,
+        id: id,
+      },
+    });
+    if (product === undefined) {
+      throw new AppError(
+        "Product not exists or its not from this supplier",
+        400
+      );
+    }
+    await productRepository.delete(product);
+
+    return "deleted";
   } catch (error) {
     throw new AppError((error as any).message, 400);
   }
