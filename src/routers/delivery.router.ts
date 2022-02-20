c1d09f9d43235228fc171771c0a1225c92f4b258
Rodrigo-DM <rodrigo.dmnr@gmail.com>
2022-02-19 21:27:19 -0300

    feat: Adding permission checking for ADM accesses

--- src/routers/delivery.router.ts
+++ src/routers/delivery.router.ts
@@ -7,6 +7,8 @@ import {
     registerDelivery,
     updateDelivery,
 } from "../controllers/delivery.controller";
+import {isAuthenticated} from "../middlewares/authentication.middleware";
+import {isAdmCheck} from "../middlewares/authorization.middleware";
 import {validate} from "../middlewares/validation.middleware";
 import {
     CreateDeliverySchema,
@@ -16,10 +18,22 @@ import {
 const router = Router();
 
 export const deliveryRouter = () => {
-    router.post("", validate(CreateDeliverySchema), registerDelivery);
-    router.get("/:id", getDelivery);
-    router.get("", listDeliverys);
-    router.delete("/:id", delDelivery);
-    router.put("/:id", validate(updateDeliverySchema), updateDelivery);
+    router.post(
+        "",
+        isAuthenticated,
+        isAdmCheck,
+        validate(CreateDeliverySchema),
+        registerDelivery
+    );
+    router.get("/:id", isAuthenticated, getDelivery);
+    router.get("", isAuthenticated, isAdmCheck, listDeliverys);
+    router.delete("/:id", isAuthenticated, isAdmCheck, delDelivery);
+    router.put(
+        "/:id",
+        isAuthenticated,
+        isAdmCheck,
+        validate(updateDeliverySchema),
+        updateDelivery
+    );
     return router;
 };
