14aba16cb07ebc08b586513a8249c14036364f91
Rodrigo-DM <rodrigo.dmnr@gmail.com>
2022-02-22 19:17:06 -0300

    Fix: Delivery Schema Correction

--- src/schemas/deliverySchema.ts
+++ src/schemas/deliverySchema.ts
@@ -3,7 +3,7 @@ import * as yup from "yup";
 export const CreateDeliverySchema = yup.object().shape({
     status: yup.string().required(),
     buy_id: yup.string().required(),
-    trunks_id: yup.string().required(),
+    trucks_id: yup.string().required(),
 });
 
 export const updateDeliverySchema = yup.object().shape({
