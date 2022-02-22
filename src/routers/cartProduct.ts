e59dec411740d36db123d3fa2f66f1e754981d1b
Rodrigo-DM <rodrigo.dmnr@gmail.com>
2022-02-21 21:47:48 -0300

    Adding see cart function

--- src/routers/cartProduct.ts
+++ src/routers/cartProduct.ts
@@ -1,11 +1,15 @@
-import { Router } from "express";
-import { createNewCart } from "../controllers/cartProduct.controller";
-import { isAuthenticated } from "../middlewares/authentication.middleware";
+import {Router} from "express";
+import {
+    createNewCart,
+    seeProductsCart,
+} from "../controllers/cartProduct.controller";
+import {isAuthenticated} from "../middlewares/authentication.middleware";
 
 const router = Router();
 
 export const cartProductRouter = () => {
-  router.post("/:productId", isAuthenticated, createNewCart);
+    router.post("/:productId", isAuthenticated, createNewCart);
+    router.get("", isAuthenticated, seeProductsCart);
 
-  return router;
+    return router;
 };
